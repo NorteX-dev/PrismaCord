2fa5641b3a90d0a8542011f585b9f67f66d746e2
RifksSch <RifksSch>
2022-05-08 10:55:04 +0800

    feat: Attachments

--- src/structures/Attachment.ts
+++ src/structures/Attachment.ts
@@ -1,1 +1,27 @@
-export class Attachment {}
+import { AttachmentOptions } from "../../lib/interfaces/AttachmentOptions";
+
+export class Attachment {
+    id: string;
+    filename: string;
+    description?: string;
+    content_type?: string;
+    size: number;
+    url: string;
+    proxy_url: string;
+    height?: number;
+    width?: number;
+    ephemeral?: boolean;
+
+    constructor(options: AttachmentOptions) {
+        this.id = options.id;
+        this.filename = options.filename;
+        this.description = options.description;
+        this.content_type = options.content_type;
+        this.size = options.size;
+        this.url = options.url;
+        this.proxy_url = options.proxy_url;
+        this.height = options.height;
+        this.width = options.width;
+        this.ephemeral = options.emphermeral;
+    }
+}
