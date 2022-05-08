ad213ea06452a795105f5841048363f03e550e94
RifksSch <RifksSch>
2022-05-09 07:49:19 +0800

    feat: created file SendOotion.ts

---
+++ lib/interfaces/SendOption.ts
@@ -0,0 +1,12 @@
+import { Attachment } from "../../src/structures/Attachment";
+import { Embed } from "../../src/structures/Embed";
+import { ActionRow } from "../../src/structures/ActionRow";
+import { Button } from "../../src/structures/Button";
+import { SelectMenu } from "../../src/structures/SelectMenu";
+
+export interface SendOption {
+    content?: string,
+    embeds?: Array<Embed>,
+    files?: Array<Attachment>,
+    components?: Array<ActionRow|Button|SelectMenu>
+}
